453e062fd1c53c4eb23dbc0277a48f8bb81953a4
unipine <unipine29@gmail.com>
2022-06-24 15:27:57 -0500

    change redirectUri

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,6 +1,6 @@
 const config = {
   appId: '5aae1770-9ee9-43c6-85a4-05242bdb221c',
-  redirectUri: 'http://localhost:3000',
+  redirectUri: 'https://nbawidget-dev.herokuapp.com',
   scopes: [
     'user.read',
     'mail.read',
